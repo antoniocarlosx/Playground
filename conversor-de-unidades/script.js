d801c405dc9dd153eaaf75513697eea621846adb
Antonio Barros <xdatabycarlos@gmail.com>
2026-08-23 16:57:35 -0300

    Conclusão do laboratorio - Lab - Validando um formulário JavaScript

--- conversor-de-unidades/script.js
+++ conversor-de-unidades/script.js
@@ -1,32 +1,47 @@
+/*
+
+A lógica é a mesma para cada uma das funções de conversão.
+
+Basicamente: 
+1. Pega o valor digitado no campo que será convertido (celsius, kg ou km)
+2. Verifica se o campo está vazio e emite um alerta
+3. Se estiver tudo certo faz a conversão para a medida desejada (fahrenheit, libras ou milhas)
+4. Exibe o resultado no campo do valor convertido
+
+*/
+
 function temperature() {
   var celsius = document.getElementById("celsius").value;
-  var fahrenheit = (celsius * 9) / 5 + 32;
 
   if (!celsius) {
     alert("Digite a temperatura em Celsius");
+    celsius.focus();
   }
+  var fahrenheit = (celsius * 9) / 5 + 32;
 
   document.getElementById("fahrenheit").value = fahrenheit;
 }
 
 function weight() {
   var kilos = document.getElementById("kilo").value;
-  var pounds = kilos * 2.2;
 
   if (!kilos) {
     alert("Digite o valor em KGs");
+    kilos.focus();
   }
+  var pounds = kilos * 2.2;
 
   document.getElementById("pound").value = pounds;
 }
 
 function distance() {
   var kms = document.getElementById("km").value;
-  var miles = kms * 0.62137;
 
   if (!kms) {
     alert("Digite o valor em KMs");
+    kms.focus();
   }
+  var miles = kms * 0.62137;
 
   document.getElementById("miles").value = miles;
 }
