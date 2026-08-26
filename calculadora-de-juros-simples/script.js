d438075a34b6713d1ee9ccd697c0e8718118fe74
Antonio Barros <xdatabycarlos@gmail.com>
2026-08-25 21:15:40 -0300

    Finalizando Script - Laboratório Prático - Calculadora de Juros Simples

--- calculadora-de-juros-simples/script.js
+++ calculadora-de-juros-simples/script.js
@@ -1,6 +1,56 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function updateRate() {
+  var rateVal = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateVal;
 }
-        
+
+const calculate = () => {
+  // Declara valores constantes para capital, taxa e tempo, pegando as respectivas ids
+  const principal = document.getElementById("principal").value;
+  const rate = document.getElementById("rate").value;
+  const years = document.getElementById("time").value;
+  const principalError = document.getElementById("principalError");
+  const timeError = document.getElementById("timeError");
+
+  event.preventDefault();
+
+  if (principal == "") {
+    principalError.textContent = "Esse campo é obrigatório!";
+    return;
+  } else {
+    principalError.textContent = "";
+  }
+
+  if (years == "") {
+    timeError.textContent = "Esse campo é obrigatório!";
+    return;
+  } else {
+    timeError.textContent = "";
+  }
+
+  var interest = (principal * years * rate) / 100;
+
+  var year = new Date().getFullYear() + parseInt(years);
+
+  var amount = parseInt(principal) + parseFloat(interest);
+
+  var result = document.getElementById("result");
+
+  if (principal <= 0) {
+    alert("Digite um valor positivo");
+    return;
+  }
+
+  if (years <= 0 || years > 10) {
+    alert("Por favor digite entre 1 e 10");
+    return;
+  }
+
+  result.innerHTML = `<p>
+    Se você depositar R$ <mark>${principal},00</mark>,
+  a uma taxa de juros de <mark>${rate}%</mark>,
+  você receberá uma quantia de R$ <mark>${amount}</mark>
+  no ano de <mark>${year}</mark>.
+
+  </p>`
+  
+};
