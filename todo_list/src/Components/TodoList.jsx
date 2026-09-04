4bb585c6927ea8cda4d0981052242cddd221e01f
Antonio Carlos <xdatabycarlos@gmail.com>
2026-09-04 15:31:45 -0300

    React: Laboratório - Aplicação de Lista de Tarefas

--- todo_list/src/Components/TodoList.jsx
+++ todo_list/src/Components/TodoList.jsx
@@ -27,17 +27,40 @@ const TodoList = () => {
             className="heading-input"
             placeholder="Enter heading"
             value={headingInput} // Vincula o valor digitado à variável de estado headinInput
-            
             // No instante que o valor de entrada muda usa o valor dela para atualizar a variavel de estado
             onChange={(e) => {
               setHeadingInput(e.target.value);
             }}
           />
-          
-          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
+
+          <button className="add-list-button" onClick={handleAddTodo}>
+            Add Heading
+          </button>
         </div>
       </div>
-      <div className="todo_main"></div>
+      <div className="todo_main">
+        {/*Mapeia cada item de tarefa e executa a função de exibir e deletar */}
+        {todos.map((todo, index) => (
+          <div key={{ index }} className="todo-card">
+            <div className="heading_todo">
+              {/*Cada Item de tarefa é redenrizado no seu card*/}
+              <h3>{todo.heading}</h3>
+              {/*Acompanhado de toda tarefa vem um botão de deletar*/}
+              <button className="delete-button-heading" onClick={() => 
+                {handleDeleteTodo(index)}}>
+                Delete Heading
+              </button>
+            </div>
+            <div className="add_list">
+              <input type="text" className="list-input"
+              placeholder="Adicionar Lista"
+              value={listInputs[index] || ''}
+              onChange={(e) => {handleListInputChange(index, e.target.value)}}/>
+              <button className="add-list-button" onClick={() => handleAddList(index)}>Adicionar Lista</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
